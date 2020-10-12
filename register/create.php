4717246f3d1265e9d6de7aa76745adcc449849f0
MohnishVS <monishcena619@gmail.com>
2020-10-12 20:01:07 +0530

    used js jquery to chk password match in frontend

--- register/create.php
+++ register/create.php
@@ -1,3 +1,4 @@
+<?php include("config.php");?>
 <html>
 
 <head>
@@ -5,9 +6,27 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
-
+<script>  
+			var check = function() {
+				if (document.getElementById('password').value == document.getElementById('confirm_password').value) {
+					document.getElementById('message').style.color = 'green';
+					document.getElementById('message').innerHTML = 'Password matching';
+				} 
+				else {
+					document.getElementById('message').style.color = 'red';
+					document.getElementById('message').innerHTML = 'Password not matching';
+				}
+			}
+			
+			var usercheck = function(){
+				var user_check_query = "SELECT username FROM user";
+				var user_mail_id = "SELECT email FROM user";
+				
+			}
+</script>
 <body>
 	<div class="container-fluid pt-3 my-3 border bg-dark text-white">
 		<div class="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-left form pt-4 my-8">
@@ -26,17 +45,19 @@
 					</div>
 					<div class="form-group">
 						<label for="Password">Password:</label><br />
-						<input type="password" name="password" class="form-control" required minlength=4 maxlength=8
+						<input type="password" name="password" class="form-control" id="password" required minlength=4 maxlength=8
 							placeholder="********"><br />
 					</div>
 					<div class="form-group">
 						<label for="Confirm Password">Confirm Password:</label><br />
-						<input type="password" name="confirm_password" class="form-control" required minlength=4 maxlength=8
-							placeholder="********"><br />
+						<input type="password" name="confirm_password" class="form-control" id="confirm_password" onkeyup='check();' required minlength=4 maxlength=8
+							placeholder="********">
+						<span id="message"></span>
 					</div>
 					<div class="form-group">
-						<input type="submit" name="register_btn" class="btn btn-primary" onsubmit="server.php" value="register"><br />
+						<input type="submit" name="register_btn" class="btn btn-primary" value="register"><br />
 					</div>
+
 				</form>
 				
 			</div>
--- register/server.php
+++ register/server.php
@@ -1,21 +1,13 @@
 <?php
     include("config.php");
-    $isValid=true; 
+	$isValid=true;
+	$error_message=""; 
 
 	if (isset($_POST['register_btn'])) {
 		$username =  $_POST['username'];
 		$email =  $_POST['email'];
 		$password = $_POST['password'];
 		$conf_password = $_POST['confirm_password'];
-	  
-		if($username == '' || $email == '' || $password == '' || $conf_password == ''){
-            $isValid = false;
-			$error_message = "Please fill all fields.";
-		}	
-		if ($password != $conf_password) {
-				$isValid=false;
-				$error_message="The two passwords do not match";
-		}
 		
 			$user_check_query = "SELECT * FROM user WHERE username='$username' OR email='$email' limit 1";
 			if($stmt = $pdo->prepare($user_check_query)){
@@ -61,7 +53,7 @@
 					$param_password = $password;
 					
 					if($stmt->execute()){
-                        $success_message="Account Created Successfully";
+						$success_message="Account Created Successfully";
 					} 
 					else{
 						$error_message = "Something went wrong. Please try again later.";
@@ -69,27 +61,7 @@
 				}
 			}
 			else{
-                $error_message=$error_message+"it is invalid";
+                $error_message="it is invalid";
 			}
         }
 ?>
-<html>
-	<head>Next page</head>
-	<meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-	<body>
-		<div class="container-sm-4 pt-3 my-3 border bg-dark text-white">
-			<div class="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-left form pt-4 my-8">
-				<div class="alert alert-danger">
-					  <strong>Error!</strong> <?=$error_message; ?>
-					  <input type="submit" name="go back" class="btn btn-primary">
-				</div>
-				<div class="alert alert-success">
-              		<strong>Error!</strong> <?=$success_message; ?>
-            	</div>
-			</div>
-		</div>
-	</body>
-</html>
