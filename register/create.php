6c92e5d056144e9c13a98e01e70efe818833d6af
Mohnish V S <40720272+MohnishVS@users.noreply.github.com>
2020-10-15 14:24:22 +0530

    Add files via upload

--- register/create.php
+++ register/create.php
@@ -1,11 +1,14 @@
+<?php include('process.php'); ?>
 <html>
-
 <head>
 	<title>Register Page</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+	<script src="script.js"></script>
 </head>
 
 <body>
@@ -13,34 +16,37 @@
 		<div class="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-left form pt-4 my-8">
 			<h2 class="display-4">Register</h2>
 			<div class="form-group">
-				<form action="server.php" method="POST" class="registerform">
+				<form method="POST" class="registerform">
 					
 					<div class="form-group">
 						<label for="Username">Username:</label><br />
-						<input type="text" name="username" class="form-control" required minlength=4 maxlength=8
-							placeholder="username"><br />
+						<input type="text" name="username" id="username" class="form-control" required minlength=4 maxlength=8
+							placeholder="username"><br /><div  id="message1"></div>
 					</div>
 					<div class="form-group">
 						<label for="email">Email:</label><br />
-						<input type="text" name="email" class="form-control" required placeholder="Email"><br />
+						<input type="text" name="email" id="email" class="form-control" required placeholder="Email"><br />
+						<div  id="message2"></div>
 					</div>
 					<div class="form-group">
 						<label for="Password">Password:</label><br />
-						<input type="password" name="password" class="form-control" required minlength=4 maxlength=8
+						<input type="password" name="password" class="form-control" id="password" required minlength=4 maxlength=8
 							placeholder="********"><br />
 					</div>
 					<div class="form-group">
 						<label for="Confirm Password">Confirm Password:</label><br />
-						<input type="password" name="confirm_password" class="form-control" required minlength=4 maxlength=8
+						<input type="password" name="confirm_password" class="form-control" id="confirm_password" onkeyup='check();' required minlength=4 maxlength=8
 							placeholder="********"><br />
+						<div  id="message"></div>
 					</div>
 					<div class="form-group">
-						<input type="submit" name="register_btn" class="btn btn-primary" onsubmit="server.php" value="register"><br />
+						<input type="submit" name="register_btn" id="reg_btn" class="btn btn-primary" value="register"><br />
 					</div>
+
 				</form>
-				
 			</div>
 		</div>
 	</div>
 </body>
+
 </html>
---
+++ register/script.js
@@ -0,0 +1,94 @@
+var check = function() {
+    if (document.getElementById('password').value == document.getElementById('confirm_password').value) {
+        document.getElementById('message').style.color = 'green';
+        document.getElementById('message').innerHTML = 'Password matching';
+    } 
+    else {
+        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').innerHTML = 'Password not matching';
+    }
+};
+
+$('document').ready(function(){
+    var username_state = false;
+    var email_state = false;
+    
+    $('#username').on('blur', function(){
+     var username = $('#username').val();
+     if (username == '') {
+         username_state = false;
+         return;
+     }
+     $.ajax({
+       url: 'process.php',
+       type: 'post',
+       data: {
+           'username_check' : 1,
+           'username' : username,
+       },
+       success: function(response){
+         if (response == 'taken' ) {
+             username_state = false;
+             document.getElementById('message1').innerHTML = 'username already exists';;
+         }
+         else if (response == 'not_taken') {
+             username_state = true;
+             document.getElementById('message1').innerHTML = 'username available';
+         }
+       }
+     });
+    });
+
+    $('#email').on('blur', function(){
+        var email = $('#email').val();
+        if (email == '') {
+            email_state = false;
+            return;
+        }
+        $.ajax({
+         url: 'process.php',
+         type: 'post',
+         data: {
+             'email_check' : 1,
+             'email' : email,
+         },
+         success: function(response){
+             if (response == 'taken' ) {
+             email_state = false;
+             document.getElementById('message2').innerHTML = 'email already exists';
+             }
+             else if (response == 'not_taken') {
+               email_state = true;
+               document.getElementById('message2').innerHTML = 'welcome new user';
+             }
+         }
+        });
+    });
+
+    $('#reg_btn').on('click', function(){
+        var username = $('#username').val();
+        var email = $('#email').val();
+        var password = $('#password').val();
+        if (username_state == false || email_state == false) {
+         $('#error_msg').text('Fix the errors in the form first');
+       }else{
+         // proceed with form submission
+         $.ajax({
+             url: 'process.php',
+             type: 'post',
+             data: {
+                 'save' : 1,
+                 'email' : email,
+                 'username' : username,
+                 'password' : password,
+             },
+             success: function(response){
+                 alert('user saved');
+                 $('#username').val('');
+                 $('#email').val('');
+                 $('#password').val('');
+             }
+         });
+        }
+    });
+   });
