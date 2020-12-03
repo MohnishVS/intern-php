3653eb52303cb7a629bb61e6860fd47550b996bc
MohnishVS <monishcena619@gmail.com>
2020-12-03 19:47:15 +0530

    composer installed

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '513a4a9c2cbf34bf838683558b6204ec5c3ebffc',
+    'name' => 'login-app/login-application',
+  ),
+  'versions' => 
+  array (
+    'login-app/login-application' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '513a4a9c2cbf34bf838683558b6204ec5c3ebffc',
+    ),
+  ),
+);
