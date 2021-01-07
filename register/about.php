9884d33e00af124914bc1e98d95d7b7739863604
MohnishVS <monishcena619@gmail.com>
2021-01-07 19:57:53 +0530

    up

--- register/about.php
+++ register/about.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once __DIR__ . '\vendor\autoload.php';;
-use stringyinflector\StringyInflector as S;
+require_once __DIR__ . '\..\vendor\autoload.php';;
+use StringyInflector\StringyInflector as S;
 $stringy = S::create( $_SESSION['name']); 
 $res = $stringy->capitalizePersonalName();
 ?>
